94004b5a9fc2c8b89807e4d265b395495a9f4aea
Andrew Wheeler <wheeler02@gmail.com>
2016-11-06 17:11:47 +0800

    basic functionality of google form working - missing validation on workshop interest

---
+++ js/google-form-submission.js
@@ -0,0 +1,8 @@
+$(function() {
+    $("input,textarea,radio,checkbox,email").not("[type=submit]").jqBootstrapValidation({});
+});
+
+
+function showhide() {
+    $('#presenter-interest').toggle();
+}
